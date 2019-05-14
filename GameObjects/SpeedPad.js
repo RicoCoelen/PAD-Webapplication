c675b41c4001daf9d3501334084acbb8e017ac60
ThomasO02 <thomas.otte02@gmail.com>
2019-05-14 16:44:23 +0200

    Pijnmeet level veranderd naar NRS schaal, berekend pijnop schaal 1-10

---
+++ GameObjects/SpeedPad.js
@@ -0,0 +1,66 @@
+class SpeedPad extends GameObject{
+  constructor(x, y, w, h, options = null){
+    // pass variables to upper class
+    super(x, y);
+
+    // save variables to object
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+
+    // add matter.js options for physics
+    this.options = options;
+
+    // add rigidbody physics to box
+    this.body = Matter.Bodies.rectangle(this.x, this.y, this.w, this.h, this.options);
+
+    // if flipped objects will be sent to the left instead
+    this.flipped = false;
+
+    this.collisions = [];
+  }
+
+
+  update(){
+    super.update();
+    this.vertices = this.body.vertices;
+
+
+
+    if(this.collisions.length >= 1){
+
+      for(let i = 0; i < this.collisions.length; i++){
+
+          if(this.vertices[0].x < this.collisions[i].bodyB.position.x && this.vertices[1].x > this.collisions[i].bodyB.position.x){
+
+            if(0 < (this.vertices[0].y + this.vertices[1].y + this.vertices[2].y + this.vertices[3].y - this.collisions[i].bodyB.position.y * 4) / 4){
+
+              Matter.Body.setVelocity(this.collisions[i].bodyB, {
+                x: this.collisions[i].bodyB.velocity.x - (this.vertices[0].x - this.vertices[1].x) / this.w,
+                y: this.collisions[i].bodyB.velocity.y - (this.vertices[0].y - this.vertices[1].y) / this.w
+              });
+            }
+          }
+        }
+      }
+    }
+
+
+  draw(){
+    //get body position
+    var pos = this.body.position;
+    var angle = this.body.angle;
+
+    // draw box
+    fill(200, 200, 0, 60);
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    rect(0, 0, this.w, this.h);
+    rect(0, -this.h / 4, this.w, this.h / 2);
+    pop();
+    fill(0);
+  }
+}
--- GameStates/PlayingState.js
+++ GameStates/PlayingState.js
@@ -9,7 +9,9 @@ class PlayingState extends GameObjectList {
     //creates the particle system
     this.reset();
 
-    this.lastCollissions = [];
+    //this.lastCollissions = [];
+
+    this.result = 0;
 
   }
 
@@ -24,18 +26,19 @@ class PlayingState extends GameObjectList {
 
     //Testing
     this.water = new Water(512, height / 2, 175, height);
+    this.speedPad = new SpeedPad(624, height * 7 / 8, 175, height / 8)
 
     //this.particleSystem = new ParticleSystem(createVector(500,500));
 
     // create the player and the cannon/line
     this.player = new Player(200, height - 150, 20, {isStatic: false, restitution: 0.99});
-    this.theCannon = new Cannon(200, height - 150, 100, this.player, 0.5);
+    this.theCannon = new Cannon(200, height - 150, 100, this.player, 0.2);
     this.tracingLine = new TracingLine(this.theCannon, this.player);
 
     // add static blocks
-    this.blocks.add(new StaticBox(400, height - 40, 25, 80));  // 1st
-    this.blocks.add(new StaticBox(625, height - 40, 25, 120)); // 2nd
-    this.blocks.add(new StaticBox(850, height - 40, 25, 160)); // 3th
+    this.blocks.add(new StaticBox(200, height - 40, 25, 80));  // 1st
+    //this.blocks.add(new StaticBox(625, height - 40, 25, 120)); // 2nd
+    //this.blocks.add(new StaticBox(850, height - 40, 25, 160)); // 3th
 
     // make static outer walls
     this.blocks.add(new Boundary(-40, height/2, 80, 2000));      // left
@@ -44,9 +47,9 @@ class PlayingState extends GameObjectList {
     this.blocks.add(new Boundary(width/2, height+40, 2000, 80)); // bottom
 
     // add text objects
-    this.texts.add(new TextGameObject(480, height - 50, "Weinig Pijn"));
-    this.texts.add(new TextGameObject(690, height - 50, "Gemmidelde Pijn"));
-    this.texts.add(new TextGameObject(935, height - 50, "Veel Pijn"));
+    this.texts.add(new TextGameObject(220, height - 50, "Geen Pijn"));
+    this.texts.add(new TextGameObject((220 + width - 100) / 2, height - 50, "Gemmidelde Pijn"));
+    this.texts.add(new TextGameObject(width - 100, height - 50, "Erg Veel Pijn"));
 
     // add the important stuff to gameobjectlist
 
@@ -56,7 +59,8 @@ class PlayingState extends GameObjectList {
     this.add(this.blocks);
     this.add(this.texts);
     this.add(this.tracingLine);
-    this.add(this.water);
+    //this.add(this.water);
+    //this.add(this.speedPad);
     //this.add(this.particleSystem);
 
   }
@@ -72,20 +76,14 @@ class PlayingState extends GameObjectList {
       this.player.visible = false;
     }
 
-    if (this.theCannon.shootingFase == 3 && this.player.body.speed < 0.30){
-      //this.reset();
-      gameEnvironment.gameStateManager.switchTo("Level");
+
+    if (this.player.body.position.y >= 580){
+      this.result = (this.player.body.position.x - 225) / (width - 245) * 10;
+      //gameEnvironment.gameStateManager.switchTo("Level");
     }
 
     // gets the array with collisions form library
     this.water.collisions = Matter.Query.collides(this.water.body, [this.player.body]);
-
-    //doesn't work
-    //use this to distinguish arrays and objects in this.children
-    //TO DO: combine with current collision check between water and player
-    for(let i = 0; i < this.children.lenth; i++){
-      if(Array.isArray(this.children[i])){
-      }
-    }
+    this.speedPad.collisions = Matter.Query.collides(this.speedPad.body, [this.player.body]);
   }
 }
